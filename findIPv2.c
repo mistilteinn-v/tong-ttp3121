3eacdab09ee913ee8653a465b8cfe3e40ccebe46
unknown <tongky20@gmail.com>
2015-08-21 04:15:32 +0800

    Modify tokenizer function. Fix tokenizer, it's working accordingly now to split string input from the popen function. Add compiled file in linux environment.

--- findIPv2.c
+++ findIPv2.c
@@ -5,16 +5,21 @@ int main(){
 
 FILE *fp;
 char returnData[64];
+char splitText[64], *s = returnData, *token;
+
 
 fp = popen("/sbin/ifconfig eth0", "r");
 
 while (fgets(returnData, 64, fp) != NULL)
 {
-    while ((splitText = strtok(returnData, " ")) != NULL) {
-    	returnData = NULL;
-    	printf(":%s:\n", splitText);
+	printf("String :%s\n", returnData);
+	s = returnData;
+	while ((token = strtok(s, " ")) != NULL) {
+        s = NULL;
+    	printf("Token:%s\n", token);
     }
 }
+
 
 pclose(fp);
 return(0);
